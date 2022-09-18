f4f05389c039fabc3c2c7104c0f79aaf50772032
Kredos <112938721+KeredosAlevel@users.noreply.github.com>
2022-09-18 14:00:25 +0300

    Fix Git

--- src/ua/zenin/hw4/Task4.java
+++ src/ua/zenin/hw4/Task4.java
@@ -8,27 +8,35 @@ import java.util.Random;
 
 public class Task4 {
     public static void main(String[] args) {
-        long[] array = getArray();
+        int[] array = getArray();
 
-        System.out.println("Initial array: " + "\n" + Arrays.toString(array));
+        System.out.println("Initial array: "  );
 
-        convertedMassive(array);
+        int [] copyArray = Arrays.copyOf(array, array.length);
+        print(copyArray);
 
-        System.out.println("Array converted: " + "\n" + Arrays.toString(array));
+
+        convertedMassive(copyArray);
+
+        System.out.println("Array converted: " + "\n" + Arrays.toString(copyArray));
     }
 
-    private static long[] getArray() {
-        long[] array = new long[2000];
+    private static int[] getArray() {
+        int[] array = new int[2000];
         Random rnd = new Random();
         for (int i = 0; i < array.length; i++) {
             array[i] = rnd.nextInt();
         }
         return array;
+
     }
-    private static void convertedMassive(long[] array) {
-        for (int i = 0; i < array.length; i++) {
+    public static void print(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+    private static void convertedMassive(int[] copyArray) {
+        for (int i = 0; i < copyArray.length; i++) {
             if (i % 2 == 0) {
-                array[i] = 0;
+                copyArray[i] = 0;
             }
         }
     }
